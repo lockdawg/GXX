c86988fdb2e7341fd4953b368523a5bbdb30a637
lockdawg <43064175+lockdawg@users.noreply.github.com>
2019-06-25 17:23:01 -0700

    Update gxx_install.sh

--- gxx_install.sh
+++ gxx_install.sh
@@ -121,6 +121,7 @@ function update_config() {
   #sed -i 's/daemon=1/daemon=0/' $CONFIGFOLDER/$CONFIG_FILE
   cat << EOF >> $CONFIGFOLDER/$CONFIG_FILE
 #bind=$NODEIP
+maxconnections=16
 xnode=1
 externalip=$NODEIP:$COIN_PORT
 masternodeprivkey=$COINKEY
